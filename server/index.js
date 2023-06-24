43432f82ccdf5514d36fb7c22f8831e3f1f38383
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-06-24 11:56:13 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+
+const CONNECTION_URL=process.env.CONNECTION_URL;
+const PORT  = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`)))
+    .catch((err)=> console.log(err));
