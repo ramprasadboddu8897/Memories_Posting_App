99b0080ca95c3d522dcf1932a195a05c70fc4cfe
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-07-01 11:07:40 +0530

    Push_3

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,22 +15,22 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const getPost = async (req, res) => { 
-    const { id } = req.params;
+// export const getPost = async (req, res) => { 
+//     const { id } = req.params;
 
-    try {
-        const post = await PostMessage.findById(id);
+//     try {
+//         const post = await PostMessage.findById(id);
         
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+//         res.status(200).json(post);
+//     } catch (error) {
+//         res.status(404).json({ message: error.message });
+//     }
+// }
 
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+    const post = req.body;
 
-    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
 
     try {
         await newPostMessage.save();
@@ -67,11 +67,21 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if(!req.userId) return res.json({mesaage:'Unauthenticated'})
+
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
     
     const post = await PostMessage.findById(id);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    const index = post.likes.findIndex((id)=>id === String(req.userId));
+
+    if (index ===-1){
+        post.likes.push(req.userId);
+    }else{
+        post.likes=post.likes.filter((id)=>id!==String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
     
     res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import cors from 'cors';
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 
 app.use('/posts',postRoutes);
+app.use('/user', userRouter);
 
 const CONNECTION_URL=process.env.CONNECTION_URL;
 const PORT  = process.env.PORT || 5000;
