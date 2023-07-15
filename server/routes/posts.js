41f51ee3a74bc1b155096b2adcc71a8f313ba817
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-07-15 11:19:00 +0530

    stage_5

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,18 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
-import auth from '../middleware/auth.js'
+import { getPosts, getPostsBySearch, getPost, createPost, updatePost, likePost,commentPost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
-router.post('/', auth,createPost);
-// router.get('/:id', getPost);
-router.patch('/:id', auth,updatePost);
-router.delete('/:id', auth,deletePost);
-router.patch('/:id/likePost', auth,likePost);
+router.get('/:id', getPost);
+
+router.post('/', auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost',auth,commentPost)
 
 export default router;
