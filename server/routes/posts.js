01fe5d80ae63fc8cd07bcbf11327ca8680f17490
Ramprasadboddu <106422103+ramprasadboddu8897@users.noreply.github.com>
2023-09-05 15:48:46 +0530

    final Changes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -13,6 +13,6 @@ router.post('/', auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
-router.post('/:id/commentPost',auth,commentPost)
+router.post('/:id/commentPost',auth,commentPost);
 
 export default router;
